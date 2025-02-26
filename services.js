2d125154b98551a2d82e6d624cab90387baf41b1
psrct <sirachet.c@gmail.com>
2025-02-26 14:31:43 +0700

    edit, delete Service

--- services.js
+++ services.js
@@ -68,6 +68,61 @@ app.post('/add-service', function (req, res) {
   });
 });
 
+  //แสดงรายการแต่ละบริการ
+app.get('/showservices/:id', function(req, res){
+  const userId = req.params.id;
+  console.log(userId);
+    db.get('SELECT * FROM Service WHERE service_id = ?', [userId], (err, row) => {
+      if (err || !row) {
+        return res.status(404).send('User not found.');
+      }
+      res.render('edit-service', { data: row });
+    });
+});
+
+  //แก้ไขข้อมูลบริการ
+app.post('/edit-service', function (req, res) {
+  console.log('Received Data:', req.body);
+  const updateData = {
+    serviceID: req.body.id,
+    serviceName: req.body.serviceName,
+    serviceCategory: req.body.serviceCategory,
+    price: req.body.price,
+    serviceDuration: req.body.serviceDuration,
+    serviceStatus: req.body.serviceStatus
+  };
+  console.log(updateData);
+  const sql = `UPDATE Service SET name = ?, category_id = ?, price = ?, duration = ?, status = ? WHERE service_id = ?`;
+  db.run(sql, [
+    updateData.serviceName,
+    updateData.serviceCategory,
+    updateData.price,
+    updateData.serviceDuration,
+    updateData.serviceStatus,
+    updateData.serviceID
+  ], (err) => {
+    if (err) {
+      console.error('Database error:', err.message);
+      return res.status(500).send('Error Update data');
+    }
+    res.redirect('/showservices');
+  });
+});
+
+  //ลบบริการ
+app.get('/deleteservice/:id', (req, res) => {
+  // req.params.id --> processes --> respond  res.*
+  let id = req.params.id;
+  const query = `DELETE FROM Service WHERE service_id='${id}'; `;
+  db.run(query, function (err) {
+      if (err) {
+          return console.error(err.message);
+      }
+      console.log(`A Service deleted.`);
+      res.redirect('/showservices');
+  });
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
