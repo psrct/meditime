d8fcb6cd006060df3c6a225aa35fb1dc7da42ad5
YoSirawit <66070314@kmitl.ac.th>
2025-03-03 02:58:14 +0700

    Make edit profile info more easier to use

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ const bcrypt = require('bcrypt');
 const db = require('./database.js');
 const session = require('express-session');
 const { checkLoggedIn, bypasslogin, isOwner, isStaff } = require('./middleware.js');
+const { json } = require('stream/consumers');
 
-
+app.use(express.json());
 app.use(express.static('webpages'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
@@ -139,11 +140,11 @@ app.get('/logout', function(req, res){
 })
 
 
-// -------------------------------- PROFILE SYSTEMS --------------------------------------------------------
+// --------------------------------  FOR PATIENT --------------------------------------------------------
 
 app.get('/profile', checkLoggedIn, function(req, res){
   console.log(`this is session for user ${req.session.user}`);
-  const sql = `select prename, firstname, lastname, birth_date, gender, username from Patients where patient_id = ${req.session.user.id} `;
+  const sql = `select prename, firstname, lastname, birth_date, gender, username, iden_id, contact_number, add_contact_number, medical_conditions from Patients where patient_id = ${req.session.user.id} `;
   db.all(sql, function(err, rows){
     console.log(rows);
     res.render('profile', {data : rows});
@@ -156,13 +157,21 @@ app.post('/profile', checkLoggedIn, function(req, res){
     fname : req.body.firstname,
     lname : req.body.lastname,
     birth_date : req.body.birth_date,
-    gender : req.body.gender
+    gender : req.body.gender,
+    iden_id : req.body.iden_id,
+    connum : req.body.contact_num,
+    add_connum : req.body.add_contact,
+    medcon : req.body.medical_conditions
   }
   const sql = `update Patients set
     prename = "${updateData.prename}",
     firstname = "${updateData.fname}",
     lastname = "${updateData.lname}",
-    gender = "${updateData.gender}"
+    gender = "${updateData.gender}",
+    iden_id = "${updateData.iden_id}",
+    contact_number = "${updateData.connum}",
+    add_contact_number = "${updateData.add_connum}",
+    medical_conditions = "${updateData.medcon}"
     where patient_id = ${req.session.user.id} `;
   db.run(sql, function(err){
     if (err) throw err;
@@ -170,14 +179,18 @@ app.post('/profile', checkLoggedIn, function(req, res){
     });
 })
 
+app.get('/', checkLoggedIn, function(req, res){
+    res.render('home');
+})
 
-// -------------------------------------- MANAGE STAFF SYSTEMS -------------------------------------------------------
+// -------------------------------------- FOR CLINIC OWNER -------------------------------------------------------
 
 
 app.get('/manage-staff', checkLoggedIn, isOwner, function(req, res){
-  const sql = `select * from Doctors`;
+  const sql = `select d.doctor_id, d.prename, d.firstname, d.lastname, d.gender, d.contact_number, d.specialty_id, s.name, d.hire_date, d.retire_date, d.username, d.password 
+  from Doctors d join Specialties s using(specialty_id)`;
   db.all(sql, function(err, rows){
-    // console.log(rows);
+    // console.log(JSON.stringify(rows));
     res.render('manage-staff', {data : rows});
     });
 })
@@ -186,24 +199,47 @@ app.get('/edit-staff/:id', checkLoggedIn, isOwner, function(req, res){
   const sql = `select * from Doctors where doctor_id = ${req.params.id} `;
   db.all(sql, function(err, rows){
     console.log(rows);
-    res.render('profile-staff', {data : rows});
+    db.all("select * from Specialties", (err, result) =>{
+      if(err) throw err;
+      var str = `{ "docinfo" : ` + JSON.stringify(rows) + `, "specialtyinfo" : ` + JSON.stringify(result) + `}`;
+      console.log(typeof(str));
+      console.log(JSON.parse(str));
+      res.render('profile-staff', {data : JSON.parse(str)});
+    })
     });
 })
 
 app.post('/edit-staff/:id', checkLoggedIn, isOwner, function(req, res){
   let updateData = {
     id : req.params.id,
-    prename : req.body.prename,
+    // prename : req.body.prename,
+    username : req.body.username,
+    password : req.body.password,
+    contact_number : req.body.contact_number,
     fname : req.body.firstname,
     lname : req.body.lastname,
     retire_date : req.body.retire_date,
-    gender : req.body.gender
+    gender : req.body.gender,
+    specialty_id : req.body.specialty_id
   }
+
+  let prename = "";
+
+  if(updateData.gender == "Male"){
+    prename = "นายแพทย์";
+  }else{
+    prename = "แพทย์หญิง";
+  }
+
   const sql = `update Doctors set
-    prename = "${updateData.prename}",
+    prename = "${prename}",
     firstname = "${updateData.fname}",
     lastname = "${updateData.lname}",
-    gender = "${updateData.gender}"
+    gender = "${updateData.gender}",
+    specialty_id = "${updateData.specialty_id}",
+    username = "${updateData.username}",  
+    password = "${updateData.password}",
+    contact_number = "${updateData.contact_number}"
     where doctor_id = ${updateData.id} `;
   db.run(sql, function(err){
     if (err) throw err;
@@ -253,35 +289,33 @@ app.post('/add-staff', checkLoggedIn, isOwner, function(req, res){
         res.redirect('/manage-staff');
 });
 
-// -------------------------------------- HOME OF EACH USER -------------------------------------------------------
-
-app.get('/', checkLoggedIn, function(req, res){
-    res.render('home');
-})
-  
-app.get('/home-staff', checkLoggedIn, isStaff, function(req, res){
-    res.render('home-staff');
+app.get('/remove-staff/:id', checkLoggedIn, isOwner, function(req, res){
+  const sql = `delete from Doctors where doctor_id = ${req.params.id} `;
+  db.run(sql, function(err){
+    if (err) throw err;
+    res.redirect("/manage-staff");
+    });
 })
 
 app.get('/showservices', checkLoggedIn, isOwner, function (req, res) {
-    const sql = `
-      SELECT s.*, sc.name AS category_name
-      FROM Service s
-      JOIN Service_categories sc
-      ON s.category_id = sc.category_id
-    `;
-  
-    db.all(sql, (err, rows) => {
-      if (err) {
-        console.error('Database error:', err.message);
-        return res.status(500).send('Error fetching data');
-      }
-      res.render('services', { data: rows });
-    });
+  const sql = `
+    SELECT s.*, sc.name AS category_name
+    FROM Service s
+    JOIN Service_categories sc
+    ON s.category_id = sc.category_id
+  `;
+
+  db.all(sql, (err, rows) => {
+    if (err) {
+      console.error('Database error:', err.message);
+      return res.status(500).send('Error fetching data');
+    }
+    res.render('services', { data: rows });
+  });
 });
-  
+
   // เพิ่มบริการใหม่
-app.post('/add-service',checkLoggedIn, isOwner, function (req, res) {
+  app.post('/add-service',checkLoggedIn, isOwner, function (req, res) {
     console.log('Received Data:', req.body);
     const addData = {
         serviceID: req.body.serviceID,
@@ -365,6 +399,14 @@ app.get('/deleteservice/:id',checkLoggedIn, isOwner, (req, res) => {
         res.redirect('/showservices');
     });
 });
+
+// -------------------------------------- FOR STAFF  -------------------------------------------------------
+
+  
+app.get('/home-staff', checkLoggedIn, isStaff, function(req, res){
+    res.render('home-staff');
+})
+
 
 app.get('/showdata',checkLoggedIn, isOwner || isStaff, function (req, res) {
     const query = 'SELECT * FROM Tasks;';
